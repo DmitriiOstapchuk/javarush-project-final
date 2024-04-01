d0998dcbb8b4dcb70653a7ada53c35543f6eaae7
Dmitrii Ostapchuk <dmit.ostapchuk@gmail.com>
2024-04-01 11:19:02 +0500

    tests for update() method with correct data was added

--- src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
+++ src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
@@ -1,29 +1,39 @@
 package com.javarush.jira.profile.internal.web;
 
 import com.javarush.jira.AbstractControllerTest;
+import com.javarush.jira.login.User;
+import com.javarush.jira.login.UserTo;
+import com.javarush.jira.profile.ProfileTo;
+import com.javarush.jira.profile.internal.ProfileMapper;
 import com.javarush.jira.profile.internal.ProfileRepository;
+import com.javarush.jira.profile.internal.model.Profile;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static com.javarush.jira.common.util.JsonUtil.writeValue;
 import static com.javarush.jira.login.internal.web.UserController.REST_URL;
 import static com.javarush.jira.login.internal.web.UserTestData.*;
 import static com.javarush.jira.profile.internal.web.ProfileTestData.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 class ProfileRestControllerTest extends AbstractControllerTest {
+    private static final String REST_URL_PROFILE = ProfileRestController.REST_URL;
     @Autowired
     private ProfileRepository profileRepository;
+    @Autowired
+    private ProfileMapper profileMapper;
 
     @Test
     @WithUserDetails(value = USER_MAIL)
     void getUserProfileTo() throws Exception {
-        perform(MockMvcRequestBuilders.get(ProfileRestController.REST_URL))
+        perform(MockMvcRequestBuilders.get(REST_URL_PROFILE))
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
@@ -33,7 +43,7 @@ class ProfileRestControllerTest extends AbstractControllerTest {
     @Test
     @WithUserDetails(value = GUEST_MAIL)
     void getGuestProfileTo() throws Exception {
-        perform(MockMvcRequestBuilders.get(ProfileRestController.REST_URL))
+        perform(MockMvcRequestBuilders.get(REST_URL_PROFILE))
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
@@ -42,7 +52,24 @@ class ProfileRestControllerTest extends AbstractControllerTest {
 
     @Test
     void getUnauthorized() throws Exception {
-        perform(MockMvcRequestBuilders.get(ProfileRestController.REST_URL))
+        perform(MockMvcRequestBuilders.get(REST_URL_PROFILE))
                 .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void update() throws Exception {
+        long user_id = 1;
+        Profile updated = getUpdated(user_id);
+        ProfileTo updatedTo = getUpdatedTo();
+        perform(MockMvcRequestBuilders.put(REST_URL_PROFILE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(writeValue(updatedTo)))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+
+        Profile dbProfileAfter = profileRepository.getExisted(user_id);
+        PROFILE_MATCHER.assertMatch(dbProfileAfter, updated);
+
     }
 }
